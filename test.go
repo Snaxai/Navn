b38099a63f377a5097515a851cd4643ae82da7e3
sander kvale <sanderkvale@sanders-mbp-3.uia.no>
2019-02-04 15:30:15 +0100

    Fixa litt på filen brur

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main(){
-        fmt.Println("Daniel, Aleksander, Herman, Henrik, Sander")
+        fmt.Println("Daniel, Aleksander, Herman, Henrik, Sander G.")
+	fmt.Println("Old Long Swine")
 
 }
