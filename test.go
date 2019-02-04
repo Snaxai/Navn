8daf98d26d8a683a2f333e67143ec46a8ca505eb
Sander <Sander@users-mbp-2.uia.no>
2019-02-04 15:31:12 +0100

    hei på deg

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main(){
-        fmt.Println("Daniel, Aleksander, Herman, Henrik, Sander")
+        fmt.Println("Daniel, Aleksander, Herman, Henrik, Sander, Sander V")
 
 }
+
